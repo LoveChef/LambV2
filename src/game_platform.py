90c9fdb387de6bb04d661a43b3bab652d6be834a
Allllexx <117825800+Allllexx@users.noreply.github.com>
2024-02-05 09:53:57 +0100

    Kommentarer i game_platform

--- src/game_platform.py
+++ src/game_platform.py
@@ -2,7 +2,14 @@ import pygame
 
 
 class Platform(pygame.sprite.Sprite):
+    """Class for platforms 
 
+    Args:
+        x (int): The x coordinate for the platform
+        y (int): The y coordinate for the platform
+        width (int): The width of the platform
+        screen_height (int): Screen height  
+    """
     def __init__(self, x, y, width, screen_height, platform_image):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(platform_image, (width, 10))
@@ -10,14 +17,17 @@ class Platform(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-    # nya variabel
         self.screen_height = screen_height
 
     def update(self, scroll) -> None:
+        """Update the platforms position
 
-        # Uppdaterar plattformens position vertikalt
+        Args:
+            scroll (int): The verticial scrolling value
+        """
+        # Update the platforms vertical position
         self.rect.y += scroll
 
-        # check if platform has gone off the screen
+        # checks if the platform has gone off the screen
         if self.rect.top > self.screen_height:
             self.kill()
