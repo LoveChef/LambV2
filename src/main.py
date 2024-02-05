b086c30a6394995874b5d454466500a3cda44d84
LoveChef <117826056+LoveChef@users.noreply.github.com>
2024-02-05 09:58:49 +0100

    Översätter kommentarer

--- src/main.py
+++ src/main.py
@@ -7,31 +7,30 @@ from game_platform import Platform
 
 pygame.init()
 
-# Spelfönstrets bredd och höjd
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+# Game width and height
+screen_width = 400
+screen_height = 600
 
-# Skapar fönstret för spelet
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Creates the game window
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('LAMB')
 
-# Sätter spelets uppdateringsfrekvens, fps = frames per second
+# Updating frequency
 clock = pygame.time.Clock()
 FPS = 60
 
-# Spel variabler
-# * SCROLL_THRESH = 200 # jag flyttar den till player classen eftersom den används bara där.
-GRAVITY = 1
-MAX_PLATFORMS = 10
+# Game variables
+gravity = 1
+max_platforms = 10
 scroll = 0
 bg_scroll = 0
 game_over = False
 score = 0
 fade_counter = 0
 
-# Definierar färgerna
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+# Defines the colors
+white = (255, 255, 255)
+black = (0, 0, 0)
 '''
 # Definerar fontsen
 font_small = font_small = pygame.font.Font(
@@ -43,19 +42,19 @@ font_big = pygame.font.Font(
 font_small = pygame.font.SysFont('Times New Roman', 20)
 font_big = pygame.font.SysFont('Times New Roman', 24)
 
-# "loadar" bilderna
+# Loads the images
 char_image = pygame.image.load(
     'assets/placeholder_charachter.png').convert_alpha()
 bg_image = pygame.image.load('assets/background.jpg').convert_alpha()
 platform_image = pygame.image.load('assets/platform.png').convert_alpha()
 
 
-# Funktion som visar texten på spelskärmen
+# Function that shows the text
 def draw_text(text, font, text_col, x, y) -> None:
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
-# Ritar ut bakgrunden
+# Draws the background
 
 
 def draw_bg(bg_scroll) -> None:
@@ -67,12 +66,12 @@ def draw_bg(bg_scroll) -> None:
 platform_group = pygame.sprite.Group()
 
 # Spelar instans
-char = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150, GRAVITY,
-              char_image, screen, SCREEN_WIDTH, platform_group)
+char = Player(screen_width // 2, screen_height - 150, gravity,
+              char_image, screen, screen_width, platform_group)
 
 # Skapar plattformen
-platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT -
-                    50, 100, SCREEN_HEIGHT, platform_image)
+platform = Platform(screen_width // 2 - 50, screen_height -
+                    50, 100, screen_height, platform_image)
 platform_group.add(platform)
 
 # Spelets main loop
@@ -91,12 +90,12 @@ while run:
         draw_bg(bg_scroll)
 
         # Generera plattformen som gubben hoppar på
-        if len(platform_group) < MAX_PLATFORMS:
+        if len(platform_group) < max_platforms:
             # jag gjorde platformerna lite större från 40, 60 till 60, 80 (kan ändras sen om det behövs)
             p_w = random.randint(60, 80)
-            p_x = random.randint(0, SCREEN_WIDTH - p_w)
+            p_x = random.randint(0, screen_width - p_w)
             p_y = platform.rect.y - random.randint(80, 120)
-            platform = Platform(p_x, p_y, p_w, SCREEN_HEIGHT, platform_image)
+            platform = Platform(p_x, p_y, p_w, screen_height, platform_image)
             platform_group.add(platform)
 
         # Uppdaterar plattformarna ju längre upp i spelet man kommer
@@ -107,19 +106,19 @@ while run:
         char.draw()
 
         # Checkart om spelet är över eller inte
-        if char.rect.top > SCREEN_HEIGHT:
+        if char.rect.top > screen_height:
             game_over = True
     else:
-        if fade_counter < SCREEN_WIDTH:
+        if fade_counter < screen_width:
             fade_counter += 5
             for y in range(0, 6, 2):
                 pygame.draw.rect(
-                    screen, BLACK, (0, y * 100, fade_counter, 100))
+                    screen, black, (0, y * 100, fade_counter, 100))
                 pygame.draw.rect(
-                    screen, BLACK, (SCREEN_WIDTH - fade_counter, (y + 1) * 100, SCREEN_WIDTH, 100))
-        draw_text('Du förlorade', font_big, WHITE, 130, 200)
-        draw_text('Poäng ' + str(score), font_big, WHITE, 130, 250)
-        draw_text('Klicka SPACE för att köra igen', font_big, WHITE, 40, 300)
+                    screen, black, (screen_width - fade_counter, (y + 1) * 100, screen_width, 100))
+        draw_text('Du förlorade', font_big, white, 130, 200)
+        draw_text('Poäng ' + str(score), font_big, white, 130, 250)
+        draw_text('Klicka SPACE för att köra igen', font_big, white, 40, 300)
         key = pygame.key.get_pressed()
         if key[pygame.K_SPACE]:
             # Återställer värdena när spelet tar slut
@@ -128,12 +127,12 @@ while run:
             scroll = 0
             fade_counter = 0
             # Flyttar tillbaka gubben till början av spelet
-            char.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+            char.rect.center = (screen_width // 2, screen_height - 150)
             # Tar bort och återställer de gamla plattformarna
             platform_group.empty()
             # Skapar en ny startplattform
-            platform = Platform(SCREEN_WIDTH // 2 - 50,
-                                SCREEN_HEIGHT - 50, 100, SCREEN_HEIGHT, platform_image)
+            platform = Platform(screen_width // 2 - 50,
+                                screen_height - 50, 100, screen_height, platform_image)
             platform_group.add(platform)
 
     # Event handelr
